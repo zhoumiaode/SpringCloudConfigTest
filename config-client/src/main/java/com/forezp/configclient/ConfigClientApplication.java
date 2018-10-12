b0ac6238fcffded318378fbd9783debfb708006c
zhoumiaode <1010467470@qq.com>
2018-10-12 17:43:06 +0800

    second

--- config-client/src/main/java/com/forezp/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/forezp/configclient/ConfigClientApplication.java
@@ -2,11 +2,14 @@ package com.forezp.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ConfigClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
 }
